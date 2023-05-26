ef3bd1a92c589cdec8b890fe9594b712bcad6bee
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-05-26 17:03:06 +0700

    Config Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
+
 function App() {
-    return <h1>Hello anh em, một bốn năm</h1>;
+    console.log(publicRoutes);
+    return (
+        <BrowserRouter>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    console.log(route);
+                    const Page = route.component;
+                    let Layout = DefaultLayout;
+
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
+
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
